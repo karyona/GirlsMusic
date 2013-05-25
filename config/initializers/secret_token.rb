f1a5cedbf6b9efe5f9293ac9740e4e2ca6cccc51
Karyona <yangnabuaa@163.com>
2013-05-25 13:09:46 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GirlsMusic::Application.config.secret_token = 'bfb79f3924cf93e5d7fcbdb3eba939972df014c6fb5268c55322ec980a371c61a3455353e341344444fcc7d7920cc798675362977895feb101434ebfaa52963e'
